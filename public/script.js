d5cba68559f52dc22f1d5f652097e75f92c8e657
GaiKOZAKI <125722953+GaiKOZAKI@users.noreply.github.com>
2024-09-01 14:42:53 +0900

    デプロイ用の修正

--- public/script.js
+++ public/script.js
@@ -254,7 +254,7 @@ submitButtonElement.onclick = async () => {
 
   async function fetchProducts() {
     try {
-      const response = await fetch('http://localhost:3000/products');
+      const response = await fetch('/products');
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
